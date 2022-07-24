f11f37a1863c5de5f7c20a39dfbad27069416edd
wallace <wsalesg@hotmail.com>
2022-07-24 16:10:02 -0300

    Estop Site

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const collapsibleMenuButton = document.querySelector("#collapsible-menu-button");
+const collapsibleMenu = document.querySelector("#collapsible-menu");
+
+collapsibleMenuButton.addEventListener("click" , function()
+{
+    collapsibleMenu.classList.toggle("menu-active");
+});
+
+collapsibleMenu.addEventListener("click" , function()
+{
+    collapsibleMenu.classList.toggle("menu-active");
+})
+
+
